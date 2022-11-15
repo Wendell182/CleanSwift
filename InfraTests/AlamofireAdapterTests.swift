4f4d8389f015b672c8ddea7b1512565dfcf79d38
wendellclaus <wendellclaus@brq.com>
2022-11-15 17:14:29 -0300

    add Infra layer

---
+++ InfraTests/AlamofireAdapterTests.swift
@@ -0,0 +1,15 @@
+//
+//  InfraTests.swift
+//  InfraTests
+//
+//  Created by Wendell Claus on 15/11/22.
+//
+
+import XCTest
+ 
+class AlamofireAdapterTests: XCTestCase {
+    func test_() {
+        
+    }
+
+}
