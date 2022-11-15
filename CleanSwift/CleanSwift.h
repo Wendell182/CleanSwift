aacdcb5aa10d194d6bdeaedebf44b62939a6ac5c
wendellclaus <wendellclaus@brq.com>
2022-11-14 22:31:00 -0300

    Initial Commit

---
+++ CleanSwift/CleanSwift.h
@@ -0,0 +1,18 @@
+//
+//  CleanSwift.h
+//  CleanSwift
+//
+//  Created by Wendell Claus on 14/11/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CleanSwift.
+FOUNDATION_EXPORT double CleanSwiftVersionNumber;
+
+//! Project version string for CleanSwift.
+FOUNDATION_EXPORT const unsigned char CleanSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CleanSwift/PublicHeader.h>
+
+
