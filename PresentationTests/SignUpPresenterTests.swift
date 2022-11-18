c35816f32340299b19936f3cb11f9029a7bae851
wendellclaus <wendellclaus@brq.com>
2022-11-18 16:19:38 -0300

    ensure SignUpPresenter shows error if no name is provided

--- PresentationTests/SignUpPresenterTests.swift
+++ PresentationTests/SignUpPresenterTests.swift
@@ -7,8 +7,57 @@
 
 import XCTest
 
-class SignUpPresenterTests: XCTestCase {
-    func test_()  {}
-        
+class SignUpPresenter {
+    private let alertView: AlertView
+    
+    init(alertView: AlertView) {
+        self.alertView = alertView
+    }
+    
+    func signUp(viewModel: SignUpViewModel) {
+        if viewModel.name == nil || viewModel.name!.isEmpty {
+            alertView.showMessage(viewModel: AlertViewModel(title: "Falha na validação", message: "O campo é obrigatório"))
+        }
+    }
+}
 
+protocol AlertView {
+    func showMessage(viewModel: AlertViewModel)
+}
+
+struct AlertViewModel: Equatable {
+    var title: String
+    var message: String
+}
+
+
+
+struct SignUpViewModel {
+    var name: String?
+    var email: String?
+    var password: String?
+    var passwordConfirmation: String?
+}
+
+
+class SignUpPresenterTests: XCTestCase {
+    func test_signUp_should_show_error_message_if_name_is_not_provided() {
+        let alertViewSpy = AlertViewSpy()
+        let sut = SignUpPresenter(alertView: alertViewSpy)
+        let signUpViewModel = SignUpViewModel(email: "any_tony@forms.com", password: "secret", passwordConfirmation: "secret")
+        sut.signUp(viewModel: signUpViewModel)
+        XCTAssertEqual(alertViewSpy.viewModel, AlertViewModel(title: "Falha na validação", message: "O campo é obrigatório"))
+    }
+}
+
+extension SignUpPresenterTests {
+    class AlertViewSpy: AlertView {
+        var viewModel: AlertViewModel?
+        
+        func showMessage(viewModel: AlertViewModel) {
+            self.viewModel = viewModel
+        }
+        
+        
+    }
 }
--- USeCasesIntegrationTests/ AddAccountIntegrationTests.swift
+++ USeCasesIntegrationTests/ AddAccountIntegrationTests.swift
@@ -11,22 +11,22 @@ import Infra
 import Domain
 
 class AddAccountIntegrationTests: XCTestCase {
-    func test_add_account() {
-        let alamofireAdapter = AlamofireAdapter()
-        let url = URL(string: "https://fordevs.herokuapp.com/api/signup")!
-        let sut = RemoteAddAccount(url: url, httpClient: alamofireAdapter)
-        let addAccountModel = AddAccountModel(name: "Wendell Claus", email: "wendell.claus@gmail.com", password: "secret", passwordConfirmation: "secret")
-        let exp = expectation(description: "waiting")
-        sut.add(addCccountModel: addAccountModel) { result in
-            switch result {
-            case .failure: XCTFail("Expect success, got \(result) instead")
-            case .success(let account):
-                XCTAssertNotNil(account.id)
-                XCTAssertEqual(account.name, addAccountModel.name)
-                XCTAssertEqual(account.email, addAccountModel.email)
-            }
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 5)
-    }
+//    func test_add_account() {
+//        let alamofireAdapter = AlamofireAdapter()
+//        let url = URL(string: "https://fordevs.herokuapp.com/api/signup")!
+//        let sut = RemoteAddAccount(url: url, httpClient: alamofireAdapter)
+//        let addAccountModel = AddAccountModel(name: "Wendell Claus", email: "wendell.claus@gmail.com", password: "secret", passwordConfirmation: "secret")
+//        let exp = expectation(description: "waiting")
+//        sut.add(addCccountModel: addAccountModel) { result in
+//            switch result {
+//            case .failure: XCTFail("Expect success, got \(result) instead")
+//            case .success(let account):
+//                XCTAssertNotNil(account.id)
+//                XCTAssertEqual(account.name, addAccountModel.name)
+//                XCTAssertEqual(account.email, addAccountModel.email)
+//            }
+//            exp.fulfill()
+//        }
+//        wait(for: [exp], timeout: 5)
+//    }
 }
