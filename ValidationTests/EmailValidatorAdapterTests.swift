53202778cebfdefb1b3d468a2aef18e9840a99a4
wendellclaus <wendellclaus@brq.com>
2022-12-01 13:31:16 -0300

    add Validation target

---
+++ ValidationTests/EmailValidatorAdapterTests.swift
@@ -0,0 +1,16 @@
+//
+//  ValidationTests.swift
+//  ValidationTests
+//
+//  Created by Wendell Claus on 01/12/22.
+//
+
+import XCTest
+
+class EmailValidatorAdapterTests: XCTestCase {
+
+    func test_() {
+        
+    }
+
+}
